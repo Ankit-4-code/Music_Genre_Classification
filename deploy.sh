84abf9f42c82558f19ce275b15bd4f68d535c8a6
Ankit Chowdhury <ankitchowdhury5@gmail.com>
2024-01-28 15:56:33 +0100

    Added test workflow for CI.

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,11 @@
 
 ## Set variables from environment
 AWS_REGION=$1
-AWS_ACCESS_KEY_ID=$2
+AWS_ACCOUNT_ID=$2
 ECR_REPOSITORY_NGINX=$3
 ECR_REPOSITORY_FLASK=$4
 ECR_REPOSITORY_BENTO=$5
 VERSION=$6
-ECR_REGISTRY=$7
 
 ## Navigate to the directory containing the docker-compose.yml file
 echo "Navigating to the Docker Compose directory..."
@@ -15,11 +14,11 @@ cd /home/ubuntu/deploy
 
 ## Log in to ECR
 echo "Logging in to Amazon ECR..."
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 ## Stop running containers and remove them along with their volumes
 echo "Stopping and removing existing Docker containers..."
-docker-compose down -v
+docker-compose down -v || echo "No running containers to stop."
 
 ## Clean up Docker resources (images, containers, networks, volumes)
 echo "Cleaning up Docker resources..."
@@ -28,9 +27,9 @@ docker system prune -a --volumes -f
 
 ## Pull images from ECR
 echo "Pulling Docker images..."
-docker pull $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NGINX:$VERSION
-docker pull $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_FLASK:$VERSION
-docker pull $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_BENTO:$VERSION
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NGINX:$VERSION
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_FLASK:$VERSION
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_BENTO:$VERSION
 
 ## Launch the containers and connect the network using docker-compose
 echo "Launching the Docker compose with docker-compose-production.yml..."
