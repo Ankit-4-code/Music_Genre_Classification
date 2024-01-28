9bea5017d93418781a282d6215fb470a12ea26fa
Ankit Chowdhury <ankitchowdhury5@gmail.com>
2024-01-28 01:39:00 +0100

    Added test workflow for CI.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ## Set variables from environment
 AWS_REGION=$1
-ECR_REGISTRY=$2
+AWS_ACCESS_KEY_ID=$2
 ECR_REPOSITORY_NGINX=$3
 ECR_REPOSITORY_FLASK=$4
 ECR_REPOSITORY_BENTO=$5
@@ -14,7 +14,7 @@ cd /home/ubuntu/deploy
 
 ## Log in to ECR
 echo "Logging in to Amazon ECR..."
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 ## Stop running containers and remove them along with their volumes
 echo "Stopping and removing existing Docker containers..."
